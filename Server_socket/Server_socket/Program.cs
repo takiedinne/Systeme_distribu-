5a18ab73130201d855eb6f9a60e88a3063591743
takiedinne <38916249+takiedinne@users.noreply.github.com>
2018-12-10 20:12:31 +0100

    ajout des fichier

---
+++ Server_socket/Server_socket/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Server_socket
+{
+    class Program
+    {
+        static Socket sck;
+        static byte[] buffer { get; set; }
+        static void Main(string[] args)
+        {   //pour recuperer l address ip actuelle
+            IPHostEntry ipHostEntry = Dns.Resolve(Dns.GetHostName());
+            IPAddress ipAddress = ipHostEntry.AddressList[0];
+            Console.WriteLine("IP=" + ipAddress.ToString());
+            //Console.Read();
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sck.Bind(new IPEndPoint(ipAddress, 1234));//pour 
+            sck.Listen(100);
+
+            Socket Client = null;
+           // Socket Client1 = null;
+            while (true)
+            {
+                Client = sck.Accept();
+
+
+                buffer = new byte[Client.SendBufferSize];
+                int bytesRead = Client.Receive(buffer);
+                byte[] formatted = new byte[bytesRead];
+
+                for (int i = 0; i < bytesRead; i++)
+                {
+                    formatted[i] = buffer[i];
+                }
+                string strdata = Encoding.ASCII.GetString(formatted);
+                Console.WriteLine(strdata);
+            }
+        }
+    }
+}
---
+++ client_totorial1/client_totorial1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace client_totorial1
+{
+    public partial class Form1 : Form
+    {
+        Socket sck;
+        string serverIp="localhost";
+        int port=8080;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+            try
+            {
+                sck.Connect(localEndPoint);
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                Console.WriteLine("unable de connect ...");
+                Console.Read();
+            }
+            byte[] data = Encoding.ASCII.GetBytes(textBox1.Text);
+
+            sck.Send(data);
+            sck.Close();
+        
+        }
+    }
+}
